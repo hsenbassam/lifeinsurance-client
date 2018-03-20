351f37b3c362e65e9d70082ba617a0bfef5f429e
hsenbassam <husseinbassam95@gmail.com>
2018-03-20 16:45:26 +0200

    UI Products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -20,17 +20,17 @@ export class ProductsComponent implements OnInit {
     productService: ProductService,
     route: ActivatedRoute) { 
 
-    productService
-      .getAll()
-      .switchMap(products => {
-      this.products = products;
-      return route.queryParamMap;
-    })
-     .subscribe(params => {
-        this.category = params.get('category');
-        this.filteredProducts = (this.category) ?
-          this.products.filter(p => p.category == this.category) : this.products;
-      });
+    // productService
+    //   .getAll()
+    //   .switchMap(products => {
+    //   this.products = products;
+    //   return route.queryParamMap;
+    // })
+    //  .subscribe(params => {
+    //     this.category = params.get('category');
+    //     this.filteredProducts = (this.category) ?
+    //       this.products.filter(p => p.category == this.category) : this.products;
+    //   });
   }
 
   ngOnInit() {
