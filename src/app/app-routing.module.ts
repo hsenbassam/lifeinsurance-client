55bd83d41f8ed444d8ee211a3050016fbcd28c58
hsenbassam <husseinbassam95@gmail.com>
2018-03-31 20:12:50 +0300

    Fixing some issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,6 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { AuthGuardService as AuthGuard } from './_services/auth-guard.service';
 import { QuoteComponent } from './quote/quote.component';
 import { CheckoutComponent } from './checkout/checkout.component';
-import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { LoginComponent } from './login/login.component';
 import { AdminAuthGuard } from './_services/admin-auth-guard.service';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
@@ -58,11 +57,6 @@ export const routes: Routes = [
     {
         path: 'payment/failure',
         component: PaymentFailureComponent,
-        canActivate: [AuthGuard]
-    },
-    {
-        path: 'my-orders',
-        component: MyOrdersComponent,
         canActivate: [AuthGuard]
     },
     { path: 'login', component: LoginComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
-import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { LoginComponent } from './login/login.component';
 import { FooterComponent } from './footer/footer.component';
@@ -76,7 +75,6 @@ import { PasswordChangeComponent } from './profile/password-change/password-chan
     ShoppingCartComponent,
     CheckoutComponent,
     PaymentSuccessComponent,
-    MyOrdersComponent,
     AdminOrdersComponent,
     LoginComponent,
     FooterComponent,
